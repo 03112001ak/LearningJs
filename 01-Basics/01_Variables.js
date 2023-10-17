a440f89b009dedeabb8a2dbd624901b076a993f9
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2023-10-17 14:20:13 +0000

    Got to know about the variables and data types.

---
+++ 01-Basics/01_Variables.js
@@ -0,0 +1,16 @@
+const accountId=12212;
+let accountEmail="adityakarki2@gmail.com";
+var accountPassword="12334";
+accountState="Arunachal Pradesh";
+let accountCity;
+
+console.table([accountId,accountEmail,accountPassword,accountState,accountCity]);
+
+
+/* We should not be using hte var data type because of the block and functional scope */
+//accountId=2312; //The value of the constant cannot be changed
+
+
+accountEmail="yourwhosewisher@gmail.com";
+accountPassword="34234";
+console.table([accountId,accountEmail,accountPassword,accountState,accountCity]);
