318e96473f7e89bc19bce278108a562511efb619
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2023-10-19 16:47:54 +0000

    Did different types of conversion

--- 01-Basics/03-ConversionOperation.js
+++ 01-Basics/03-ConversionOperation.js
@@ -1,3 +1,6 @@
+/* Need of this is because lets say we got a number in a input but we want
+the operation to be done in a string, hence the need of the Conversion */
+
 let score="Aditya";
 
 console.log(typeof score);
@@ -11,3 +14,12 @@ let name=String(valueInNumber);
 console.log(name);  //Its output is also the NaN.
 
 console.log(valueInNumber);
+
+let check="33abc";
+let conversion=Number(check);
+console.log(typeof conversion);//its output is number
+console.log(conversion); //output is NaN
+
+let TypeOfNull=null;
+let conversion1=Number(TypeOfNull);
+console.log(conversion1);// Its output is 0
