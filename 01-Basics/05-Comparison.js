71ccf3ec75a4d514ce391a488b278290a56bcfcb
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2023-10-20 15:48:43 +0000

    Learned about the comparision

---
+++ 01-Basics/05-Comparison.js
@@ -0,0 +1,10 @@
+//some normal comparision
+console.log(1>2);
+console.log(1<=2);
+console.log(1==2); //does not check the data type in this case
+console.log(1===2); //also checks the data type of both the value in this case
+
+
+//console.log("2">1); this type of comparision is not reccommended because of unpredictable output but gives some output
+//console.log(null>=0); again not reccommended
+//console.log(undefined<=0); nor reccommended;
