c09abed6c6617bd21c3ac5382ac8d05986a7fe1b
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2023-10-21 16:15:22 +0000

    Learning about the memories in javascript

---
+++ 01-Basics/Memories.js
@@ -0,0 +1,4 @@
+// In JS there are two types of memories
+//  Stack and Heap
+//  Stack is  used by Primitive data types that is why a copy of the original value is provided
+//  Heap is used by the Non-Primitive data type that is why the address of the original value is given changes can  be made in the original value itself
