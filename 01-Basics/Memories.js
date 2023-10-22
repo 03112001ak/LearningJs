aa059b6618c63b2a39d2256a0f90e7dc5ed7771a
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2023-10-22 15:35:43 +0000

    Learned about the stack memory

--- 01-Basics/Memories.js
+++ 01-Basics/Memories.js
@@ -2,3 +2,8 @@
 //  Stack and Heap
 //  Stack is  used by Primitive data types that is why a copy of the original value is provided
 //  Heap is used by the Non-Primitive data type that is why the address of the original value is given changes can  be made in the original value itself
+let MyFirstName="Aditya";
+let anotherName=MyFirstName;
+anotherName="Aditya";
+console.log(MyFirstName);
+console.log(anotherName);
