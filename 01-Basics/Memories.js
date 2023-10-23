d66c5e8cda50ced73fe7c4eabd15634f26886e90
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2023-10-23 13:00:00 +0000

    Learned about the heap memory

--- 01-Basics/Memories.js
+++ 01-Basics/Memories.js
@@ -4,6 +4,17 @@
 //  Heap is used by the Non-Primitive data type that is why the address of the original value is given changes can  be made in the original value itself
 let MyFirstName="Aditya";
 let anotherName=MyFirstName;
-anotherName="Aditya";
+anotherName="Babu";
 console.log(MyFirstName);
 console.log(anotherName);
+
+let userOne={
+    email:"adityakarki@gmail.com",
+    age:22
+};
+let userTwo=userOne;
+userTwo.email="aditya@gmail.com";
+console.log(userOne.email);//output is aditya@gmail.com
+console.log(userTwo.email);//output is aditya@gmail.com
+//above two console logs tells us that Non-Primitive data types uses Heap memory which is a call by 
+//reference, so any changes made will be reflected in the original copy itselflet MyFirstName="Aditya";
