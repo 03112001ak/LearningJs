04eaf8143f6a2b8a879b02e9ee2d6eb54f1a5dee
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2023-10-25 03:35:36 +0000

    Learned about some methods using math

--- 01-Basics/05-Comparison.js
+++ 01-Basics/05-Comparison.js
@@ -7,4 +7,4 @@ console.log(1===2); //also checks the data type of both the value in this case
 
 //console.log("2">1); this type of comparision is not reccommended because of unpredictable output but gives some output
 //console.log(null>=0); again not reccommended
-//console.log(undefined<=0); nor reccommended;
+//console.log(undefined<=0); not reccommended;
--- 01-Basics/09-Nums&Maths.js
+++ 01-Basics/09-Nums&Maths.js
@@ -11,3 +11,29 @@ console.log(balance.toFixed(2));//its output is 400.00 it adds 2 0's after decim
 const FloatNumber=123.3933;
 console.log(FloatNumber.toPrecision(4)); //Its output is 123.4 works as set precision operator in C++
 
+const hundreds=100000000;
+console.log(hundreds.toLocaleString()); //puts commas in between the numbers to make it more readable by default American place value system
+console.log(hundreds.toLocaleString('en-IN')); //This makes it in Indian place value system
+
+
+
+//Maths library comes default with the javascript language
+
+console.log(typeof(Math)); //Math is an object
+console.log(Math.abs(-4));//Changes -ve values to +ve not the other way around
+console.log(Math.round(4.6));//If the value is more than 5 after decimal it will round off the to 1 more than that
+console.log(Math.ceil(4.2));//If the value is slightly greater than the number eg 4.2>4 than its output is going to be 5
+console.log(Math.floor(4.7)); //It is the opposite of ceil its output is going to be 4
+
+console.log(Math.random());//It gives random values between 0&1 0 & 1 both are not included in this
+
+console.log(Math.floor(Math.random()*10));//Here the o/p can also be 0 because it will give the floor value
+//if we want to avoid 0 from the output than we need to add 1 to it
+console.log(Math.floor((Math.random()*10)+1));
+
+
+//let say we want a random numbers in a certain range 
+
+let min=10;
+let max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
