54714c5d19e8bdd24516672f63b841141d63fefb
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2023-10-31 03:40:05 +0000

    Learned more about the objects

--- 02-Basics/03-Object.js
+++ 02-Basics/03-Object.js
@@ -16,3 +16,39 @@ const anotherUser={ //objects are made up of key value pairs and keys are treate
 
 //two ways of accessing the individual key value pairs inside an object
 console.log(anotherUser.name);
+console.log(anotherUser["name"]);//If you compare line 18 and 19, you will see name is in inverted commas in this one because keys are treated like strings but using dot operator we don't need to use it
+//we should be using line number 19 code
+
+//reasons for using the code for line number 19
+
+const declaringSymbol=Symbol("mySym1");//this code creates a new symbol with the description mySym1 and assigns it to the variable
+//const declaringSymbol=Symbol["mySym1"];//This code attempts to access a non-existent property on the Symbol object, which when logged will output undefined
+console.log(declaringSymbol);
+console.log(typeof declaringSymbol);
+
+
+const understandingLine19={
+    "name":"Aditya", //key is deliberately given in string 
+     [declaringSymbol]:"anotherKey" //this is how a key whose data type is symbol is given a value  
+}
+console.log(understandingLine19["name"]);//we can not use dot operator in here because as we see above name is in double quotes 
+console.log(understandingLine19[declaringSymbol]);// Here declaringSymbol is a key of type symbol hence square bracket has to be used
+console.log(declaringSymbol);//Here declaringSymbol is a variable which holds a value
+
+//The above written are some few reasons why we should be ignoring the dot operator to retrieve any key value pair in the object
+
+
+//Changing the values in an object
+understandingLine19["name"]="Karki";
+console.log(understandingLine19["name"]);
+
+//We can also freeze an object as written below
+//Object.freeze(understandingLine19);//The object understandingLine19 cannot be overwritten from now
+
+understandingLine19.greetings=function(){
+    console.log("Hi welcome to JavaScript");
+}
+console.log(understandingLine19.greetings); //output is  [Function (anonymous)]
+console.log(understandingLine19.greetings());//output is Hi welcome to JavaScript and in another line undefined is coming
+
+//[Function (anonymous)] means function has not been executed but reference of a function has come
