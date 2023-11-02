b3cb606a188ccec934651ae2c059112c4898c03a
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2023-11-02 04:39:21 +0000

    Learned about IIFE

---
+++ 03-Basics/04-IIFE.js
@@ -0,0 +1,21 @@
+// Immediately Invoked Function Expression (IIFE) means function should be executed immediately after written
+// because global variable can get polluted because of the local scopes hence to avoid that we use IIFE 
+
+
+function chai(){
+    console.log("Connected");
+}
+chai();
+
+
+//The above code can also be written as follows using IIFE
+//We know that bracket after chai indicates execution
+//The whole function should wrapped inside the brackets and after that at last comes the brackets for function execution followed by semicolon (semicolon is very important)
+(function chai(){ //this is named IIFE
+    console.log("Connected");
+})(); //To end the above function it is necessary to add semicolon at the end in the IIFE
+
+//same thing can also be written inside the arrow function
+( (name) =>{ //unnamed IIFE
+    console.log(`Another connected ${name}`)}
+    )("Aditya");
