38d2ff031e531dcb72d689795bede75d36f5b6f6
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2023-11-03 11:37:36 +0000

    Learned about if else statements

---
+++ 04-ControlFlow/one.js
@@ -0,0 +1,21 @@
+//if statement, let sunderstand with only one example
+if(2=="2"){ //here the first 2 is of type number and other is string, this condition is supposed to be false but the code gets executed because of wrong operator, the correct operator is given in line number 5
+    console.log(`Code is executed`);
+}
+if(2==="2"){ //this code will not execute because === operator also checks the data type 
+    console.log("Code not executed");
+}
+
+//Lets again understand the scopes within the if statements
+const score=100;
+if(score){
+    let power="fly";
+    console.log(`Power is to: ${power}`);
+}
+//console.log(`Power is to: ${power}`);//This will give power because the variable power is not accessible from outside the scope of if statement
+
+if(score){
+    var power1="run";
+    console.log(`Power is to: ${power1}`);
+}
+console.log(`Power is to: ${power1}`);//This won't give any error because var has global scope hence we should not use it
