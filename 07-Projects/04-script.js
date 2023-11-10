aa32cdb9b18e5682cd18307ab7e162b131ddebff
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2023-11-10 05:25:55 +0000

    Learned a lot in this project, built a game

---
+++ 07-Projects/04-script.js
@@ -0,0 +1,22 @@
+const button=document.querySelector('.button');
+const previousGuesses=[];
+//let guess=10;
+//while(guess>0){
+    button.addEventListener('click',()=>{
+        const input=parseInt(document.querySelector('input').value);
+        if(isNaN(input)){
+            alert('Please Enter a Valid Number');
+        }
+        else{
+            let random=Math.floor((Math.random()+1)*100);
+            previousGuesses.push(input);
+            document.querySelector('.guess').innerText=previousGuesses;
+            if(random===input){
+                alert('You ve won the game');
+                
+            }
+            guess--;
+            //document.querySelector('.value').innerHTML.replace(guess);
+        }
+    })
+//}
