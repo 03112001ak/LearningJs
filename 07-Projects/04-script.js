030ed008104210172c38f6365ead8226f31ef958
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2023-11-13 16:25:12 +0000

    Trying to do some improvements

--- 07-Projects/04-script.js
+++ 07-Projects/04-script.js
@@ -1,22 +1,34 @@
-const button=document.querySelector('.button');
-const previousGuesses=[];
-//let guess=10;
-//while(guess>0){
-    button.addEventListener('click',()=>{
-        const input=parseInt(document.querySelector('input').value);
-        if(isNaN(input)){
-            alert('Please Enter a Valid Number');
-        }
-        else{
-            let random=Math.floor((Math.random()+1)*100);
-            previousGuesses.push(input);
-            document.querySelector('.guess').innerText=previousGuesses;
-            if(random===input){
-                alert('You ve won the game');
+// const button=document.querySelector('.button');
+// const previousGuesses=[];
+// //let guess=10;
+// //while(guess>0){
+//     button.addEventListener('click',()=>{
+//         const input=parseInt(document.querySelector('input').value);
+//         if(isNaN(input)){
+//             alert('Please Enter a Valid Number');
+//         }
+//         else{
+//             let random=Math.floor((Math.random()+1)*100);
+//             previousGuesses.push(input);
+//             document.querySelector('.guess').innerText=previousGuesses;
+//             if(random===input){
+//                 alert('You ve won the game');
                 
-            }
-            guess--;
-            //document.querySelector('.value').innerHTML.replace(guess);
-        }
-    })
-//}
+//             }
+//             guess--;
+//             //document.querySelector('.value').innerHTML.replace(guess);
+//         }
+//     })
+// //}
+
+
+//random number generation
+function randomNo(){
+    let random=Math.floor((Math.random()+1)*100);
+}
+
+function NumberChecker(){
+    let inputHolder=document.getElementsByTagName('input');
+    console.log(inputHolder);
+}
+NumberChecker();
