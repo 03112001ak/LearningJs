6c95812343a003c915b932e031902e598abb58cb
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2023-11-19 08:18:38 +0000

    Learned about the callBack using async functions

--- 10-Advance_1/callBacks.js
+++ 10-Advance_1/callBacks.js
@@ -14,3 +14,13 @@ function sum(num1, num2, myCallBack){//function sent as arguement inside another
 sum(1,2,myDisplayer);
 
 //The use of callBack is mostly understood while doing asynchronous programming where one function  has to wait for another function(like waiting for a file to load), it says by the time I call this function back do this thing
+
+
+
+//so in the code below the function learningCallBack function will print second because it will wait in the queue for 5 seconds because of the setTimeOut function and the console.log will print first
+function learningCallBacks(){
+    console.log('You Know I am learning callBacks and it will print second');
+}
+setTimeout(learningCallBacks,5000);
+
+console.log('This will print first');
