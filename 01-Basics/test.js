798003d437954bbbcefb14ffe9e9cf3316ef0e63
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2024-01-08 08:29:31 +0530

    Update test.js

--- 01-Basics/test.js
+++ 01-Basics/test.js
@@ -1,1 +1,6 @@
+//There is not much difference between .txt and .js file both of them can be treated as source code. Both of them contains text.
+//But when we write console, this has a special meaning which is defined inside the .js file
+//If we use .js file to be interpreted it'll interpret each line with a predefined set of keywords or syntax and will throw according to that
+//If we use .txt file to be interpreted it'll directly throw error by saying 'I don't accept .txt file' or in computer terms 'Invalid or unexpected token'
+
 console.log("Aditya Karki")
