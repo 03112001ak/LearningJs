c6d296f587dacdcf61e560339773b1af89f065e7
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2024-01-13 11:06:00 +0530

    Update 10-Dates.js

--- 01-Basics/10-Dates.js
+++ 01-Basics/10-Dates.js
@@ -10,8 +10,8 @@ console.log(myDate.toDateString());//Wed Oct 25 2023 is the output
 
 console.log(myDate.toLocaleString());// 10/25/2023, 3:44:10 AM is the output 
 
-const myCreatedDate=new Date(2023,10, 3);
-console.log(`My Created Date is ${myCreatedDate}`);
+const myCreatedDate=new Date(2023,0, 3);//Count of the month always starts with 0, 0 is for January
+console.log(`My Created Date is ${myCreatedDate}`);//My Created Date is Tue Jan 03 2023 00:00:00 GMT+0530
 
 console.log(myCreatedDate.toString());
 console.log(myCreatedDate.toLocaleString());
@@ -19,8 +19,8 @@ console.log(myCreatedDate.toLocaleString());
 const myAnotherCreatedDate=new Date(2023,10,3,24,0,0);
 console.log(myAnotherCreatedDate.toLocaleString());
 
-const anotherFormatOfDate=new Date("11-03-2001"); //we have defined our own format as MM/DD//YYYY;
-console.log(anotherFormatOfDate);
+const anotherFormatOfDate=new Date("01-30-2001"); //we have defined our own format as MM/DD//YYYY;
+console.log(anotherFormatOfDate);//Tue Jan 30 2001 00:00:00 GMT+0530 //In string we've to specify month from 1-12, 1 for Jan & 12 for Dec
 
 let myTimeStamp=Date.now();
 console.log(myTimeStamp); //It is millisecond value from 1st Jan 1970 till today
@@ -37,7 +37,7 @@ console.log(newDate);
 console.log(newDate.getMonth()+1);//by default the count starts with 0 hence not to make end user confuse we added 1 to it
 console.log(newDate.getDay());
 
-
+//Below is a very important method using which we can customize our dates
 console.log(newDate.toLocaleString('default',{
     weekday: "long"
 }))
