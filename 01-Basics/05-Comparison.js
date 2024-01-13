47a55b16918eb92bc54430bac32fa1854147414a
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2024-01-13 08:46:47 +0530

    Update 05-Comparison.js

--- 01-Basics/05-Comparison.js
+++ 01-Basics/05-Comparison.js
@@ -5,6 +5,17 @@ console.log(1==2); //does not check the data type in this case
 console.log(1===2); //also checks the data type of both the value in this case
 
 
-//console.log("2">1); this type of comparision is not reccommended because of unpredictable output but gives some output
+//console.log("2">1); here the string 2 gets automatically converted to number and outputs true but this type of comparision is not reccommended because of unpredictable output but gives some output
 //console.log(null>=0); again not reccommended
 //console.log(undefined<=0); not reccommended;
+
+//The below code should be avoided at all cost
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//true
+//The reason behind the above unpredictable result is because equality check(==) and comparision(>=,<,>,>=) work differently.
+//Comparision convers null to a number, treating as 0. That's why null>=0 is true and null>0 is false
+
+console.log(undefined==0);//false 
+console.log(undefined<=0);//false because undefined gets converted to NaN in number
+console.log(undefined>=0);//false because undefined gets converted to NaN in number
